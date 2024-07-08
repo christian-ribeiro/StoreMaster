3003dc64dbce07bca8511cc37cc36eb5dcaa7fa1
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2024-07-07 21:24:12 -0300

    cr - add BCrypt (Not working - Stackoverflow exception)

---
+++ src/StoreMaster.Domain/Extensions/CryptographyExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace StoreMaster.Domain.Extensions
+{
+    public static class CryptographyExtension
+    {
+        public static string HashPassword(this string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(this string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
